90733fcf3ebf7cbc1637dc1c836174ede11cc981
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-12-22 13:02:10 -0500

    Add hamburger button click/cancel functionality

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const div = document.createElement('div');
+const grayBgDiv = document.createElement('div');
+let hamburgerBttn = document.querySelector(".menu-bars");
+
+hamburgerBttn.addEventListener('click', () => {
+  div.id = 'overlay';
+  div.classList.add('overlay');
+  grayBgDiv.id = 'overlay-menu-bg';
+  grayBgDiv.classList.add('overlay');
+  div.innerHTML = '<img src="./media/cancel.png" class="cancelBttn"></img>'
+    + '<ul class="mobile-menu-ul">'
+    + '<li class="mobile-menu-li"><a href="#section-1">Porfolio</a></li>'
+    + '<li class="mobile-menu-li"><a href="#section-2">About</a></li>'
+    + '<li class="mobile-menu-li"><a href="#section-3">Contact</a></li>'
+    + '</ul>';
+  document.body.appendChild(div);
+  document.body.appendChild(grayBgDiv);
+  div.style.display = 'block';
+  grayBgDiv.style.display = 'block';
+
+  // Add listener for closing menu
+  const mobileMenu = document.querySelector('.cancelBttn');
+  mobileMenu.addEventListener('click', () => {
+    div.remove();
+    grayBgDiv.remove();
+  });
+
+  // Adding listeners to li tags
+  const sections = document.querySelectorAll('.mobile-menu-li');
+  for (let i = 0; i < sections.length; i += 1) {
+    sections[i].addEventListener('click', () => {
+      div.remove();
+      grayBgDiv.remove();
+    });
+  }
+    
+});
